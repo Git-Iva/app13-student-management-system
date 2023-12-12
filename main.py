4112c7a9c1ba4bd94dfbded75865ee662c926371
iva <ivanela.lazarova@gmail.com>
2023-12-12 14:41:04 +0800

    Populate the table with data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import sys
 from datetime import datetime
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, \
-    QGridLayout, QPushButton, QMainWindow, QTableWidget
+    QGridLayout, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
+import sqlite3
+
 
 
 class MainWindow(QMainWindow):
@@ -23,16 +25,28 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         # Self below referring to the MainWindow instance
         # Table specified as central widget
         self.setCentralWidget(self.table)
 
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+
+        # Resets table, to avoid duplicate data
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                print(row_data)
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 
 app = QApplication(sys.argv)
-age_calculator = MainWindow()
-age_calculator.show()
+main_window = MainWindow()
+main_window.show()
+main_window.load_data()
 sys.exit(app.exec())
