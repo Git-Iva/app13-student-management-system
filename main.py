987e4e1ae8b05e2700c37777a5a8cc32ad4c713a
iva <ivanela.lazarova@gmail.com>
2023-12-13 15:28:08 +0800

    Add the toolbar and toolbar icons

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-from PyQt6.QtWidgets import QApplication, QLineEdit,QPushButton, \
+from PyQt6.QtWidgets import QApplication, QLineEdit, QPushButton, \
     QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, \
-    QComboBox
-from PyQt6.QtGui import QAction
+    QComboBox, QToolBar
+from PyQt6.QtGui import QAction, QIcon
 from PyQt6.QtCore import Qt
 import sys
 import sqlite3
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
+        self.setMinimumSize(800, 600)
 
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
         edit_menu_item = self.menuBar().addMenu("&Edit")
 
         # Add sub-items
-        add_student_action = QAction("Add Student", self)
+        add_student_action = QAction(QIcon("icons/add.png"), "Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
@@ -24,7 +26,7 @@ class MainWindow(QMainWindow):
         help_menu_item.addAction(about_action)
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
-        search_action = QAction("Search", self)
+        search_action = QAction(QIcon("icons/search.png"), "Search", self)
         edit_menu_item.addAction(search_action)
         search_action.triggered.connect(self.search)
 
@@ -35,24 +37,36 @@ class MainWindow(QMainWindow):
         # Table specified as central widget
         self.setCentralWidget(self.table)
 
+        # Create toolbar and add toolbar elements
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        # Adds toolbar to the window
+        self.addToolBar(toolbar)
+
+        toolbar.addAction(add_student_action)
+        toolbar.addAction(search_action)
+
+
     def load_data(self):
         connection = sqlite3.connect("database.db")
         result = connection.execute("SELECT * FROM students")
-
         # Resets table, to avoid duplicate data
         self.table.setRowCount(0)
         for row_number, row_data in enumerate(result):
+            # Inserts an empty row at a particular index in table
             self.table.insertRow(row_number)
             for column_number, data in enumerate(row_data):
+                # setItem populates cells of inserted row with actual data
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
 
     def insert(self):
-        # Instantiates insert  and search dialog classes
+        # Instantiates insert dialog class
         dialog = InsertDialog()
         dialog.exec()
 
     def search(self):
+        # Instantiate search dialog class
         dialog = SearchDialog()
         dialog.exec()
 
@@ -63,7 +77,6 @@ class InsertDialog(QDialog):
         self.setWindowTitle("Insert Student Data")
         self.setFixedWidth(300)
         self.setFixedHeight(300)
-
         layout = QVBoxLayout()
 
         # Add student name widget
@@ -82,12 +95,11 @@ class InsertDialog(QDialog):
         self.mobile.setPlaceholderText("Mobile")
         layout.addWidget(self.mobile)
 
-        self.setLayout(layout)
-
         # Add a submit button
         button = QPushButton("Submit")
         button.clicked.connect(self.add_student)
         layout.addWidget(button)
+
         self.setLayout(layout)
 
     def add_student(self):
@@ -101,7 +113,6 @@ class InsertDialog(QDialog):
         connection.commit()
         cursor.close()
         connection.close()
-        main_widow.load_data()
 
 
 class SearchDialog(QDialog):
@@ -127,14 +138,10 @@ class SearchDialog(QDialog):
 
     def search(self):
         name = self.student_name.text()
-        # Access the table of main_window object of QTable widget type
-        # Qtable widget has findItems method
-        # The Qt flag matches the name string
         # Gives a generator object to be iterated upon
         items = (main_window.table.findItems
                 (name, Qt.MatchFlag.MatchFixedString))
         for item in items:
-            print(item)
             # Represent cell coordinates which are then selected
             main_window.table.item(item.row(), 1).setSelected(True)
 
